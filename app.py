59aedd9a1482f0bdc8a10a052bf17b5ac5c8b0c5
Cadavinci <cadavincinfo@gmail.com>
2025-04-26 19:21:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.markdown("""
         text-align: center;
     }
     .stButton button {
-        background-color: white;
+        background-color: grey;
         color: white;
         font-weight: bold;
         border-radius: 8px;
